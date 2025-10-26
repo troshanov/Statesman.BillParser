c1d30eeab608315da579abb1a77157309a52998c
Stoyan Troshanov <troshanov994@gmail.com>
2025-10-27 00:02:08 +0200

    Disable add button when type is Letter; Optimize default Marker values;

--- Statesman.BillParser.Shared/Models/BillElements/BillElement.cs
+++ Statesman.BillParser.Shared/Models/BillElements/BillElement.cs
@@ -2,7 +2,17 @@
 
 public class BillElement
 {
+    private string? _marker;
+
     public required BillElementType Type { get; set; }
-    public required string Text { get; set; } = string.Empty;
-    public required string Marker { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string Marker 
+    {
+        get => _marker ??= Type switch
+        {
+            BillElementType.Letter => "a",
+            _ => "1"
+        };
+        set => _marker = value;
+    }
 }
